5e9d09e2d56ccd1cbf4269e231e2fb4537e1d132
oxifinch <oxifinch@protonmail.com>
2021-06-27 09:52:25 +0200

    moved l index variable one step up, deleted psuedocode in meta.rs

--- src/meta.rs
+++ src/meta.rs
@@ -14,26 +14,15 @@ impl ChapterData {
         }
     }
     pub fn fetch_tag_from_line(&mut self, line: &str) {
-        // loop {
-        //     find "{{" and "}}" 
-        //     match => 
-        //        1. create a copy of line
-        //        2. delete until and including {{ 
-        //        3. delete from and including }}
-        //        4. Make a string of the result and add it to self
-        //     
-        //     no more matches => break
-        // }
-
         let mut current_line = String::from(line);
         let mut l: usize;
         let mut r: usize;
         loop {
             match &current_line.find("{{") {
                 Some(lb) => {
+                    l = lb.to_owned();
                     match &current_line.find("}}") {
                         Some(rb) => {
-                            l = lb.to_owned();
                             r = rb.to_owned() + 2;
                             let full_tag = &line[l..r];
                             let tag = &full_tag[2..full_tag.len() - 2];
